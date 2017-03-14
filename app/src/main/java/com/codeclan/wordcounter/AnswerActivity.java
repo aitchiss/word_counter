3e0b0160f197ea9ef10df1f544bddb409934a4e6
Suzanne <suzanne.aitchison@gmail.com>
2017-03-14 20:40:49 +0000

    start again button added

--- app/src/main/java/com/codeclan/wordcounter/AnswerActivity.java
+++ app/src/main/java/com/codeclan/wordcounter/AnswerActivity.java
@@ -3,11 +3,13 @@ package com.codeclan.wordcounter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class AnswerActivity extends AppCompatActivity {
 
     TextView answerText;
+    TextView answerBreakdownText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,13 @@ public class AnswerActivity extends AppCompatActivity {
         answerText = (TextView) findViewById(R.id.answer_text);
         answerText.setText("Number of words in your text: " + String.valueOf(answer));
 
+        answerBreakdownText = (TextView) findViewById(R.id.answer_breakdown_text);
+        answerBreakdownText.setText(wordCounter.countOccurences(wordsToCount));
+
+    }
+
+    public void onClickBackToMainScreen(View button){
+        Intent intent = new Intent(this, WordCounterActivity.class);
+        startActivity(intent);
     }
 }
